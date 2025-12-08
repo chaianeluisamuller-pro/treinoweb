ccf06c0724ed2eedbe791c3cf1c4c61060fe526d
chaianeluisamuller-pro <chaianeluisamuller@gmail.com>
2025-12-08 19:30:09 -0300

    text_show_popupalert

---
+++ blank.js
@@ -0,0 +1,7 @@
+// Essa função faz aparecer um alerta
+
+window.alert('Hello');
+
+// Essa função pega o elemento pelo id e faz aparecer um texto desejado
+
+document.getElementById('myH').textContent= 'Welcome';
